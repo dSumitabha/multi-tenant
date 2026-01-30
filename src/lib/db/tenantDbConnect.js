061d49e5273499f0fd06444e0433810f6e77ee20
sumitabha4 <sumitabha4@gmail.com>
2026-01-30 17:39:21 +0000

    readyState 2 check to restirct initail connection issue

--- src/lib/db/tenantDbConnect.js
+++ src/lib/db/tenantDbConnect.js
@@ -7,14 +7,22 @@ export async function getTenantConnection(dbName) {
     if (!dbName) throw new Error("DB name missing");
 
     if (connections[dbName]) {
-        return connections[dbName];
+        const existing = connections[dbName];
+
+        if (existing.readyState === 2) {
+            await existing.asPromise();
+        }
+
+        return existing;
     }
 
     const uri = `${process.env.MONGODB_BASE_URI}/${dbName}`;
 
-    const conn = await mongoose.createConnection(uri, {
+    const conn = mongoose.createConnection(uri, {
         bufferCommands: false,
     });
+
+    await conn.asPromise();
 
     connections[dbName] = conn;
     return conn;
